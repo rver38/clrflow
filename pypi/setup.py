9be99ebe67cde5acd4922cab0a44fe50907e0b82
rver <rverflow@gmail.com>
2025-12-14 16:18:56 +0100

    placeholder pypi package

---
+++ pypi/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="clrflow",
+    version="0.0",
+    packages=["clrflow"],
+    author="rver",
+    author_email="rverflow@gmail.com",
+    description="placeholder for clrflow"
+)
